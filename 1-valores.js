676a8b949917754f6e8a498a28c376722ee8ff3d
Luis Ariza <luismanuelariza@hotmail.com>
2022-01-08 14:12:33 -0500

    Valores y types

---
+++ 1-valores.js
@@ -0,0 +1,23 @@
+// Tipos primitivos
+
+40   // Números 
+3.14
+
+"Luis Manuel"  // Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+
+'Luis Manuel'
+
+true   // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+false
+
+null   // Valores vacíos (Empty values), es como un placeholder (espacio reservado para algún valor) de algún valor faltante.
+
+undefined
+
+
+/* ================================================================ */ 
+
+// Tipos de objetos o no primitivos
+
+[1, 2, 3, 4] // corchetes // arrays
+{ nombre: 'Luis' }  // llaves // objetos
